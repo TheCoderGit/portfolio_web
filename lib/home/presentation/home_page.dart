39b0c409f08cbfdd904f40a2bdbe0dc022511f66
Ahmed Hassan Salem <74409660+TheCoderGit@users.noreply.github.com>
2023-05-19 19:45:15 +0300

    add web folder

--- lib/home/presentation/home_page.dart
+++ lib/home/presentation/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//FIRSTSTAGE
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -19,7 +20,14 @@ class _HomePageState extends State<HomePage> {
               .textTheme
               .headlineLarge
               ?.copyWith(color: Colors.red),
-        )
+        ),
+        Text(
+          'this is the profile page',
+          style: Theme.of(context)
+              .textTheme
+              .headlineMedium
+              ?.copyWith(color: Colors.red),
+        ),
       ],
     );
   }
